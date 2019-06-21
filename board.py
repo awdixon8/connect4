628384f1a9debb78e211b1bea9b51f8c20e921b2
Austin Dixon <austinwbdixon@gmail.com>
2019-06-21 19:50:47 -0400

    adding tests

--- board.py
+++ board.py
@@ -1,41 +1,57 @@
-class Board():
-
+class Conn4Board(object):
     def __init__(self):
         self.moves = 0
         self.columns = 7
         self.rows = 6
+        self.aval_moves = [self.rows - 1 for y in range(self.columns)]
 
     def create_board(self):
-        self.matrix = [[' ' for y in range(self.columns)] for x in range(self.rows)]
+        self.matrix = [[' ' for y in range(self.columns)]
+                       for x in range(self.rows)]
 
-    def get_board(self):
+    def get_board_state(self):
+        return self.matrix
+
+    def print_board(self):
         for row in self.matrix:
             print("|".join(col for col in row))
             # for col in row:
             # print(col, end='|')
 
     def get_moves(self):
-        self.aval_moves = []
+        return self.aval_moves
 
     def make_move(self, column):
-        self.matrix[0][int(column)] = 'x'
-        self.get_moves()
+        try:
+            if self.aval_moves[column] != '':
+                if self.moves % 2 == 0:
+                    self.matrix[self.aval_moves[column]][int(column)] = 'x'
+                else:
+                    self.matrix[self.aval_moves[column]][int(column)] = 'o'
+                self.aval_moves[column] = self.aval_moves[column] - 1
+                self.moves += 1
+            else:
+                raise (AssertionError, 'That is not an available move!')
+        except IndexError as e:
+            print('That is not a valid move.')
 
     def is_game_won(self):
         result = False
         x_wins = False
         o_wins = False
         for row in self.matrix:
-            if " " in row:
+            if self.moves < 7:
                 break
-            elif "x" in row:
-                if len(set(row)) == 1:
-                    x_wins = True
-            elif "o" in row:
-                if len(set(row)) == 1:
-                    o_wins = True
-            else:
-                print('Draw')
+            # elif set((x+1)(y)):
+            #     break
+            # elif set((x)(y+1)):
+            #     break
+            # elif set((x+1)(y+1)):
+            #     break
+            # elif set((x-1)(y-1)):
+            #     break
+            elif row[0] == 'x' and row[0] == 'x' and row[0] == 'x' and row[0] == 'x':
+                x_wins = True
         result = x_wins or o_wins
         print('result is %s' % result)
         return result
@@ -43,12 +59,12 @@ class Board():
 
 def next_turn():
     move = raw_input('Where do you want to move? ')
-    board.make_move(move)
-    board.get_board()
+    board.make_move(int(move))
+    board.print_board()
 
 
-board = Board()
+board = Conn4Board()
 board.create_board()
-board.get_board()
-while (not board.is_game_won()) and board.moves <= board.size[0] * board.size[1]:
+board.print_board()
+while (not board.is_game_won()) and board.moves <= board.columns * board.rows:
     next_turn()
